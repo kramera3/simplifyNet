b5db548ca3f48e6d816b7561113c5e3160a8011b
AMMercier <46980768+AMMercier@users.noreply.github.com>
2020-09-03 17:08:48 -0400

    Spielman Srivastava Sparse Technique
    Added:
    - Random Network Generation (ER, Config, SBM, Random Geometric)
    - Misc. Network/Matrix Tools
    - Spielman-Srivastava method of sparsification using effective resistances
    - - Some separate interest may be in a quick approximation of effective resistance for all node pairs also added

---
+++ RandomNetworkGen.R
@@ -0,0 +1,166 @@
+# Author: Alexander M. Mercier
+
+# Create a variety of random networks, saving as either edge list or adj matrix
+
+library(tidyr)
+library(fields)
+
+# Generate an Erdos-Renyi Model
+# Input:
+# n - the number of nodes
+# p - probability of adding an edge
+# Output:
+# A or E - adj matrix or edge list of ER network
+ER_gen <- function(n, p, self_edge = FALSE){
+  A = matrix(0L, nrow = n, ncol = n)
+  for (i in 1:n){
+    for (j in 1:n){
+      if (i <= j) {
+        set.seed(001)
+        theta = runif(1, min = 0, max = 1)
+        if (theta < p){
+          if (self_edge == TRUE){
+            A[i, j] = 1
+          } else if (i != j){
+            A[i, j] = 1
+          }
+        }
+      }
+    }
+  }
+  A = A + t(A)
+
+  return(A)
+}
+
+
+# Generate a Configuration Model
+# Input:
+# k - degree list
+# Output:
+# A - adj matrix of Configuration network
+ConfigNetwork_gen <- function(k){
+  if (Reduce('+', k) %% 2 != 0){stop("Please use a degree distribution that sums to an even number")} # The sum of the degrees must be even
+  L = list()
+  A = matrix(0L, nrow = length(k), ncol = length(k))
+  for (i in 1:length(k)){
+    for (j in 1:k[[i]]){
+      L = append(L, i) # Create stub list
+    }
+  }
+  set.seed
+  L = sample(L) # Randomly shuffle stublist
+  while (length(L) > 0) {
+    n1 = L[[1]]
+    L[1] = NULL
+    n2 = L[[1]]
+    L[1] = NULL
+    if (n1 == n2){
+      A[n1, n2] = 2 + A[n1, n2] # Self edges have an added degree of two
+    } else {
+      A[n1, n2] = A[n2, n1] = 1 + A[n1, n2]
+    }
+  }
+
+  return(A)
+}
+
+
+# Create a random partition of nodes with given community sizes
+# Input:
+# L - node list
+# s - list of community sizes
+# Output:
+# P - list of lists; a partition based on L and s
+l_g_partition <- function(L, s){
+  P = list()
+  cs = 1
+  for (i in 1:length(s)){
+    C = list()
+    for (j in 1:s[[i]]){
+      n = cs
+      #L[[1]][j] = NULL
+      C = append(C, n) # Add community partition to a list of partitions
+      cs = cs + 1
+    }
+    P = append(P, list(C))
+  }
+
+  return(P)
+}
+
+
+# Create a SBM
+# Input:
+# s - list of community sizes
+# W - matrix of probabilities where W_ij is the prob of connecting community i and j
+# Output:
+# A - adj matrix of SBM network
+SBM_gen <- function(s, W, self_edge = FALSE){
+  n = Reduce('+', s) # Find total number of nodes
+  A = matrix(0L, nrow = n, ncol = n)
+  nodelist = list(1:n) # Create a node list
+  P = l_g_partition(nodelist, s) # Create partition of node list
+  for (i in 1:length(P)){
+    for (j in 1:length(P)){
+      C1 = P[[i]]
+      C2 = P[[j]]
+      for (x in 1:length(C1)){
+        for (y in 1:length(C2)){
+          if (x <= y){
+            p = W[i, j]
+            theta = runif(1, min = 0, max = 1)
+            if (theta < p){
+              n = C1[[x]]
+              m = C2[[y]]
+              if (n != m){
+                A[n, m] = 1 + A[n, m]
+                if (i == j){
+                  A[n, m] = 0 + A[n, m]
+                }
+              } else if (self_edge == TRUE) {
+                A[n, n] = 2 + A[n, n]
+                if (i == j){
+                  A[n, m] = 0 + A[n, m]
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  A = A + t(A)
+
+  return(A)
+}
+
+
+# Generate random undirected geometric graph
+# Input:
+# n - the number of nodes
+# r - minimum dist to add edge
+# Output:
+# A - the adj matrix of the graph
+# pos - dict of lists, each entry the x, y coordinate
+GeoRan_gen <- function(n, r){
+  A = matrix(0L, nrow = n, ncol = n)
+  pos = data.frame(matrix(NA, nrow = n, ncol = 2))
+  pos$X1 = runif(n, min = 0, max = 1)
+  pos$X2 = runif(n, min = 0, max = 1)
+  dist_m = rdist(x1 = pos[,1:2])
+  for (i in 1:n){
+    for (j in 1:n){
+      if (i < j) {
+        if (dist_m[i,j] <= r){
+          A[i, j] = A[j, i] = 1
+        }
+      }
+    }
+  }
+
+  return(list(A,pos))
+}
+
+
+
---
+++ SpielmanSrivastava_Sparse.R
@@ -0,0 +1,157 @@
+# Author: Alexander M. Mercier
+
+# From Spielman and Srivastava 2011
+library(igraph)
+library(cPCG)
+
+source('Matrix_Mod.R')
+
+
+# Compute random \pm 1\sqrt(k) Bernoulli matrix where k=24log(n)/epsilon^2
+# Used to fulfill Johnson-Lindenstrauss lemma
+# From Spielman and Srivastava 2011
+# Input:
+# m - Number of edges
+# epsilon - List of weights (default none)
+# Output:
+# Q - Random \pm 1/sqrt(k) Bernoulli matrix
+QGen <- function(m, epsilon){
+  k = ((24 * log(m))) / (epsilon ^ 2)
+  k_d = ceiling(k)
+  x = 1 / sqrt(k)
+  Q = matrix(sample(c(-x,x), size = k_d * m, replace = TRUE),
+             nrow = k_d,
+             ncol = m)
+  #Q = as(Q, 'sparseMatrix') #Sparse is larger than non sparse matrix
+
+  return(Q)
+}
+
+
+# Approximately find effective resistances for edges of a graph
+# Based on algorithm from Spielman-Srivastava (2011)
+# Input:
+# network - adj matrix or edge list
+# epsilon - parameter to control accuracy
+# Output:
+# T - matrix of effective resistances for the edges in elist
+EffR <- function(network, epsilon, n = NULL){
+  if (nrow(network) != ncol(network)) {
+    network = Elist_Mtrx(network, n)
+  }
+  A = as(network, 'sparseMatrix')
+  B = sVIM(network)
+  L = as(laplacian_matrix(graph_from_adjacency_matrix(A), normalized = FALSE), 'matrix')
+  W = WDiag(network)
+  Q = QGen(nrow(B), epsilon)
+  Y = as((Q %*% (sqrt(W)) %*% B), 'matrix')
+  Z = matrix(NA, nrow = nrow(Y), ncol = ncol(Y))
+  for (i in 1:nrow(Y)){
+    Y_v = matrix(Y[i, ])
+    Z[i,] = cgsolve(L, Y_v)
+  }
+  R = matrix(0L, nrow = nrow(A), ncol = ncol(A))
+  for (i in 1:nrow(A)){
+    for (j in 1:nrow(A)){
+      if (i != j) R[i, j] = abs(sum((Z[,i] - Z[,j])^2))
+    }
+  }
+
+  return(R)
+}
+
+
+# Normalize probs such that sum(probs)=1
+# Input:
+# P - list of probs
+# Output:
+# P_n - list of probs that sum to 1
+normprobs <- function(P){
+  p_f = 1/ sum(P)
+  P_n = c()
+  for (p in P){
+    P_n = append(P_n, p_f * p)
+  }
+
+  return(P_n)
+}
+
+
+# Create a list of edge R_eff
+# Input:
+# R - matrix of R_effs
+# A - adj matrix
+# Output:
+# R_v - vector of edge R_eff
+EffR_List <- function(R, network, n = NULL){
+  #if (nrow(network) != ncol(network)){
+  #  A = Elist_Mtrx(network, n = n)
+  #}
+  R_v = c()
+  A[upper.tri(A)] = 0
+  R[upper.tri(R)] = 0
+  for (i in 1:nrow(A)){
+    for (j in 1:nrow(A)){
+      if (A[i,j] > 0){
+        R_v = append(R_v, R[i,j])
+      }
+    }
+  }
+
+  return(R_v)
+}
+
+
+# Create a effective resistance sparsifer
+# From Spielman and Srivastava 2008
+# Input:
+# adj - Adj matrix
+# q - number of samples
+# R - Matrix of effective resistances #(other types of edge importance in the future, possibly)#
+# Output:
+# H - effective resistance sparsifer adj matrix
+EffRSparse <- function(network, q, R, n = NULL){
+  if (nrow(network) == ncol(network)){
+    n = nrow(network)
+    A = Mtrx_Elist(network)
+  }
+  W_list = A[,3]
+  R_list = EffR_List(R, Elist_Mtrx(A, n))
+  P = c()
+  for (i in 1:nrow(A)){
+    w_e = W_list[i]
+    R_e = R_list[i]
+    P = append(P, (w_e * R_e) / (n - 1))
+  }
+  P_n = normprobs(P)
+  A = cbind(A, P_n)
+  C = A[sample(nrow(A), size = q, replace = TRUE, prob = P_n),]
+  H = matrix(0L, nrow = n, ncol = n)
+  for (j in 1:q){
+    w_e = C[j, 3]
+    p_e = C[j, 4]
+    H[C[j,1], C[j,2]] = (w_e / (q * p_e)) + H[C[j,1], C[j,2]]
+  }
+  H = H + t(H)
+
+  return(H)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
